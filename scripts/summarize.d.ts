3eecfd30cd991182b54bb67c63759b2a9326aa02
Shubham Soni <shubxam@gmail.com>
2022-02-15 17:41:31 +0530

    Initial commit

---
+++ scripts/summarize.d.ts
@@ -0,0 +1,53 @@
+/** JSON API type */
+export interface OkrApi {
+  /** OKR key-value pair with year and data */
+  years: {
+    name: number;
+    progress: number;
+    success: number;
+    quarters: {
+      name: number;
+      progress: number;
+      success: number;
+      objectives: {
+        name: string;
+        progress: number;
+        success: number;
+        key_results: {
+          name: string;
+          target_result: number;
+          current_result: number;
+          progress: number;
+          success: number;
+        }[];
+      }[];
+    }[];
+  }[];
+}
+
+/** OKR item based on ../schema.json */
+export interface OkrItem {
+  /** Year */
+  year: number;
+
+  /** Quarter */
+  quarter: 1 | 2 | 3 | 4;
+
+  /** Objectives for this quarter */
+  objectives: {
+    /** Name of objective */
+    name: string;
+
+    /** Key results for this objective */
+    key_results: {
+      /** Name of key result */
+      name: string;
+
+      /** Target value */
+      target_result: number;
+
+      /** Currently achieved value */
+      current_result: number;
+    }[];
+  }[];
+}
